2ef5cfc248cd8e93a410d996c6c783254c4c7d86
leshiysoft <leshiysoft@yandex.ru>
2020-10-08 17:02:16 +0300

    Качающаяся сцена

---
+++ VRTrainer/Assets/$TheProject/Scenes/Pitching/Pitching.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Pitching : MonoBehaviour
+{
+    public enum ScenePitcher { IsScene, IsPitcher };
+
+    public ScenePitcher scenePitcher = Pitching.ScenePitcher.IsScene;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float time = 0;
+
+    public float xAmplitude = 1;
+    public float xfrenq = 1;
+    public float zAmplitude = 1;
+    public float zfrenq = 1.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scenePitcher == ScenePitcher.IsScene)
+        {
+            transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+            time += Time.deltaTime;
+            transform.rotation = Quaternion.EulerAngles(xAmplitude*Mathf.PI/180*Mathf.Sin(xfrenq*time), 0f, zAmplitude * Mathf.PI / 180 * Mathf.Sin(zfrenq * time));
+        } else
+        {
+            transform.position = new Vector3(gameObject.transform.parent.position.x -player.position.x, transform.position.y, gameObject.transform.parent.position.z - player.position.z);
+        }
+    }
+}
