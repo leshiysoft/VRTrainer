298d8b81841b41d3931b3f1731ea48b92cf7a982
leshiysoft <leshiysoft@yandex.ru>
2020-11-02 16:50:53 +0300

    Перетаскивание

--- VRTrainer/Assets/$TheProject/Scripts/DiscreetMovable.cs
+++ VRTrainer/Assets/$TheProject/Scripts/DiscreetMovable.cs
@@ -10,10 +10,13 @@ public class DiscreetMovable : MonoBehaviour
     protected Interactable interactable;
     protected Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.DetachFromOtherHand;
 
+    public float discreetStep = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
         interactable = GetComponent<Interactable>();
+        gameObject.SetActive(false);
     }
 
     protected virtual void HandAttachedUpdate(Hand hand)
@@ -22,10 +25,15 @@ public class DiscreetMovable : MonoBehaviour
         //Vector3 newPos = new Vector3(Mathf.Round(theT.x * 10) / 10, Mathf.Round(theT.y * 10) / 10, Mathf.Round(theT.z * 10) / 10);
         //transform.position = newPos;
         transform.position = hand.transform.position;
+        //transform.rotation = hand.transform.rotation;
 
         if (hand.IsGrabEnding(this.gameObject))
         {
             hand.DetachObject(gameObject);
+            Vector3 theT = hand.transform.position;
+            float dStep = 1.0f / discreetStep;
+            Vector3 newPos = new Vector3(Mathf.Round(theT.x * dStep) / dStep, Mathf.Round(theT.y * dStep) / dStep, Mathf.Round(theT.z * dStep) / dStep);
+            transform.position = newPos;
         }
     }
 
--- VRTrainer/Assets/$TheProject/Scripts/HandOperated.cs
+++ VRTrainer/Assets/$TheProject/Scripts/HandOperated.cs
@@ -1,22 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
+//[RequireComponent(typeof(Interactable))]
 public class HandOperated : MonoBehaviour
 {
-public Transform PositionHand;
-    public Transform RotationHand;
+    public Transform PositionHand;
+    //public Transform RotationHand;
 
     Vector3 PositionOffset;
+
+    float nearZone = 0.5f;
+
+    //protected Interactable interactable;
 
     private void Start()
     {
         PositionOffset = transform.position - PositionHand.position;
+        //interactable = GetComponent<Interactable>();
     }
 
     void Update()
     {
         transform.position = PositionHand.position + PositionOffset;
-        transform.rotation = RotationHand.rotation;
+        //transform.rotation = RotationHand.rotation;
+        bool leftNear = Vector3.Distance(Player.instance.leftHand.transform.position, PositionHand.position) < nearZone;
+        bool rightNear = Vector3.Distance(Player.instance.rightHand.transform.position, PositionHand.position) < nearZone;
+        PositionHand.gameObject.SetActive(leftNear || rightNear);
     }
+
+    /*private void OnHandHoverBegin()
+    {
+        PositionHand.gameObject.SetActive(true);
+    }
+
+
+    //-------------------------------------------------
+    private void OnHandHoverEnd()
+    {
+        PositionHand.gameObject.SetActive(false);
+    }*/
+
+
 }
