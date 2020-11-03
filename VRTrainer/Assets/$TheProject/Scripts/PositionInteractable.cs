32ec95c10953dfea23aa7c92181b0366ed1a1bd5
leshiysoft <leshiysoft@yandex.ru>
2020-11-03 15:42:43 +0300

    Двери

---
+++ VRTrainer/Assets/$TheProject/Scripts/PositionInteractable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class PositionInteractable : MonoBehaviour
+{
+    protected Interactable interactable;
+    protected Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.DetachFromOtherHand;
+
+    public float discreetStep = 0.1f;
+
+    Vector3 AttachingOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    protected virtual void HandAttachedUpdate(Hand hand)
+    {
+        //Vector3 theT = hand.transform.position;
+        //Vector3 newPos = new Vector3(Mathf.Round(theT.x * 10) / 10, Mathf.Round(theT.y * 10) / 10, Mathf.Round(theT.z * 10) / 10);
+        //transform.position = newPos;
+        transform.position = hand.transform.position + AttachingOffset;
+        //transform.rotation = hand.transform.rotation;
+
+        if (hand.IsGrabEnding(this.gameObject))
+        {
+            hand.DetachObject(gameObject);
+            Vector3 theT = transform.position;
+            float dStep = 1.0f / discreetStep;
+            Vector3 newPos = new Vector3(Mathf.Round(theT.x * dStep) / dStep, Mathf.Round(theT.y * dStep) / dStep, Mathf.Round(theT.z * dStep) / dStep);
+            transform.position = newPos;
+        }
+    }
+
+
+    protected virtual void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            AttachingOffset = transform.position - hand.transform.position;
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VRTrainer/Assets/$TheProject/Scripts/StartWater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWater : MonoBehaviour
+{
+
+    public GameObject water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            water.SetActive(!water.activeSelf);
+        }
+    }
+}
