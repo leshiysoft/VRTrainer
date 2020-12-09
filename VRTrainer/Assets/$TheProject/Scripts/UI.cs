6af0821fe07dc1077dd30796b53d12b534aa088d
leshiysoft <leshiysoft@yandex.ru>
2020-12-09 15:09:30 +0300

    GUI скрытие/раскрытие

---
+++ VRTrainer/Assets/$TheProject/Scripts/UI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+
+    public GameObject mainPanel;
+    public GameObject unwrapButton;
+
+    public void Unwrap()
+    {
+        mainPanel.SetActive(true);
+        unwrapButton.SetActive(false);
+    }
+
+    public void Wrap()
+    {
+        mainPanel.SetActive(false);
+        unwrapButton.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainPanel.SetActive(false);
+        unwrapButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
